5d201758d6ef47975d7a7169af6a4421d6527eae
Nora Archer <narcher20@stuy.edu>
2021-01-11 19:35:38 -0500

    not working:

--- DriverRadix.java
+++ DriverRadix.java
@@ -1,29 +1,41 @@
 public class DriverRadix {
   public static void main(String[] args) {
 
+    // for (String num : args) {
+    //   int n = Integer.parseInt(num);
+    //   System.out.println("Number is: "+num+", and length is: "+Radix.length(n));
+    //   System.out.println("Ones place: "+Radix.nth(n, 0)+", Tens place: "+Radix.nth(n, 1)+", and Hundreds place: "+Radix.nth(n, 2));
+    //   System.out.println();
+    // }
+    //
+    // SortableLinkedList gouda = new SortableLinkedList(); //gouda.add("cheese");
+    // SortableLinkedList zer0 = new SortableLinkedList(); SortableLinkedList on1e = new SortableLinkedList();
+    // zer0.add(50); zer0.add(340);  zer0.add(0);
+    // on1e.add(-1); on1e.add(71);  on1e.add(-891);
+    // SortableLinkedList tw02 = new SortableLinkedList(); SortableLinkedList thre3 = new SortableLinkedList();
+    // thre3.add(5333); thre3.add(43);  thre3.add(03);
+    //
+    // SortableLinkedList[] Buckets;
+    // Buckets = new SortableLinkedList[4];
+    // Buckets[0] = zer0;  Buckets[1] = on1e;  Buckets[2] = tw02; Buckets[3] = thre3;
+    // Radix.merge(gouda, Buckets);
+    // System.out.println();
+    // System.out.println("Merger: "+gouda);
+    // for (int i = 0; i < Buckets.length; i++) {
+    //   System.out.println("What remains in the buckets: "+Buckets[i]);
+    // }
+
+    SortableLinkedList input = new SortableLinkedList();
     for (String num : args) {
       int n = Integer.parseInt(num);
-      System.out.println("Number is: "+num+", and length is: "+Radix.length(n));
-      System.out.println("Ones place: "+Radix.nth(n, 0)+", Tens place: "+Radix.nth(n, 1)+", and Hundreds place: "+Radix.nth(n, 2));
-      System.out.println();
+      input.add(n);
     }
-
-    MyLinkedList gouda = new MyLinkedList(); //gouda.add("cheese");
-    MyLinkedList zer0 = new MyLinkedList(); MyLinkedList on1e = new MyLinkedList();
-    zer0.add("50"); zer0.add("340");  zer0.add("0");
-    on1e.add("-1"); on1e.add("71");  on1e.add("-891");
-    MyLinkedList tw02 = new MyLinkedList(); MyLinkedList thre3 = new MyLinkedList();
-    thre3.add("5333"); thre3.add("43");  thre3.add("03");
-
-    MyLinkedList[] Buckets;
-    Buckets = new MyLinkedList[4];
-    Buckets[0] = zer0;  Buckets[1] = on1e;  Buckets[2] = tw02; Buckets[3] = thre3;
-    Radix.merge(gouda, Buckets);
     System.out.println();
-    System.out.println("Merger: "+gouda);
-    for (int i = 0; i < Buckets.length; i++) {
-      System.out.println("What remains in the buckets: "+Buckets[i]);
-    }
+    System.out.println("Unsorted: "+input+" "+input.size());
+    Radix.radixSortSimple(input);
+    System.out.println("\nSorted: "+input+" "+input.size());
+
+
 
 
   }
--- Radix.java
+++ Radix.java
@@ -15,10 +15,61 @@ public class Radix {
     return word.length();
   }
 
-  public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
+  public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
     for (int i = 0; i < buckets.length; i++) {
       original.extend(buckets[i]);
     }
+  }
+
+  public static void radixSortSimple(SortableLinkedList data) {
+    int passes = 1;
+    System.out.println();
+    for (int i = 0; i < passes; i++) {
+      SortableLinkedList[] Buckets;
+      Buckets = new SortableLinkedList[10];
+      for (int m = 0; m < 10; m++) {
+        SortableLinkedList temp = new SortableLinkedList();
+        Buckets[m] = temp;
+      }
+      for (int j = 0; j < data.size(); j++) {
+        int here = data.get(j).intValue();
+        // System.out.println("Number to be sorted: "+here);
+        // System.out.println("Looking at the "+(int)Math.pow(10, i)+"s place.");
+        // System.out.println("The appropriate bucket: "+nth(here, i));
+        //Buckets[ nth(here, i) ] = Buckets[nth(here, i)].add(here);
+        SortableLinkedList aBucket = Buckets[nth(here, i)];
+        aBucket.add( data.get(j) );
+        Buckets[nth(here, i)] = aBucket;
+        //System.out.println("Here's the bucket: "+Buckets[nth(here, i)] );
+        if (i == 0 && length(here) > passes) passes = length(here);
+      }
+      SortableLinkedList empty = new SortableLinkedList();
+      merge(empty, Buckets);
+      data = empty; //i can't do this, but if i loop backwards above and remove from data, the bucket sorting wont be in order
+      //merge(data, Buckets)
+      System.out.println("We've just ordered it by: "+(int)Math.pow(10, i)+"s place.");
+      System.out.println("Here's the merged data set: "+data);
+    }
+  }
+
+  public static void radixSort(SortableLinkedList data) {
+    //allow for negatives :{, DEFINITELY call the mrthod above
+    // SortableLinkedList negData, put this in simple sort as positive (do this while you're dividing the total data set), then flip and re-neg
+    // SortableLinkedList posData, put this in simple sort
+    // SortableLinkedList[] Positives;
+    // Positives = new SortableLinkedList[1];
+    // Positives[0] = posData;
+    // merge(negData, Positives);
+    // data = negData;
+  }
+
+  private static SortableLinkedList flipIt(SortableLinkedList data) {
+    return data;
+  }
+
+  private static void clear(SortableLinkedList data) {
+    SortableLinkedList temp = new SortableLinkedList();
+    data = temp; //this just... has no effect when called
   }
 
 
